5f13d3909772baa80b03510c5570bb5c5e2be7a3
jaytrive <113422452+jaytrive@users.noreply.github.com>
2023-02-28 11:41:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 //your code here
-let input=document.getElementById('evaluatedText')
-let output=document.getlementById('wordCount')
-let arr=input.value.split(' ')
-output.innerText=arr.length
+let output=document.getElementById('wordCount')
+
+function realTimeChange(){
+	let spaces=0
+	let str=document.getElementById('evaluatedText').value
+	let arr=str.split(' ')
+	arr.forEach((value) => {
+		if(value=='') spaces++
+	})
+	output.innerText=arr.length-spaces
+}
+
